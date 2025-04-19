0bba6820e60fe6ff6ef4e259c2d851ed7ab691b0
Javier Kadjo <javiernaim@hotmail.com>
2025-04-19 17:39:03 -0300

    Creacion de columna TIPO_HOGAR ok.

--- src/creacion_columnas_hogares.py
+++ src/creacion_columnas_hogares.py
@@ -30,5 +30,46 @@ if inp == 'si':
         print(i, columna)
 # ---------------------------------------------------
 
-for fila in dataset: #imprime una columna
-    print(fila['PONDIH'])
+columna_ix_tot = [fila["IX_TOT"] for fila in dataset] #list comprehension - IX TOT indica la cantidad de personas en el hogar
+
+def parseo_datos_cant_personas_hogar(inf):
+    """"
+    Clasifica la cantidad de personas en un hogar según IX_TOT:
+    1  → 'Unipersonal'
+    2-4 → 'Nuclear'
+    5+ → 'Extendido'
+    """
+    try:
+        valor = int(inf)
+    except:
+        print('Valor inválido')
+    
+    if valor == 1:
+        return 'Unipersonal'
+    elif 2 <= valor <= 4:
+        return 'Nuclear'
+    elif valor >= 5:
+        return 'Extendido'
+    else:
+        return 'Desconocido'
+
+def agregar_columna(csv_inicial, csv_final):
+    with csv_inicial.open(mode='r', encoding='utf-8') as f_inicial:
+        leerDic = csv.DictReader(f_inicial, delimiter = ';')
+        filas = []
+        
+        for fila in leerDic:
+            fila['TIPO_HOGAR'] = parseo_datos_cant_personas_hogar(fila.get('IX_TOT', ''))
+            filas.append(fila)
+
+
+    encabezados = list(filas[0].keys())
+
+    with csv_final.open(mode='w', encoding='utf-8', newline='') as f_final:
+        escritor = csv.DictWriter(f_final, fieldnames=encabezados, delimiter=';')
+        escritor.writeheader()
+        escritor.writerows(filas)
+        
+    print(f"Archivo guardado con columna 'TIPO_HOGAR': {csv_final}")
+    
+agregar_columna(archivo, archivo)
